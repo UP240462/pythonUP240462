636f2d97247d933e95fd486f182e93136aaa37a4
UP240462 <up240462@alumnos.upa.edu.mx>
2025-02-22 15:42:28 -0600

    holaaa day4

---
+++ day4_clau.py
@@ -0,0 +1,18 @@
+#1 Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+word1 = "Thirty"
+word2 = "Days"
+word3 = "Of"
+word4 = "Python"
+finalPhrase = word1 + " " + word2 + " " + word3 + " " + word4
+print(finalPhrase)
+
+#2 Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+w1 = "Codding"
+w2 = "For"
+w3 = "All"
+finalWord = w1 + " " + w2 + " " + w3
+print(finalWord)
+
+#3 Declare a variable named company and assign it to an initial value "Coding For All".
+company = int("Coding For All")
+print(company)
