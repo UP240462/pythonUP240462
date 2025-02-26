004b5c88861c9434508d9d4d8522840bcc9bb367
UP240462 <up240462@alumnos.upa.edu.mx>
2025-02-26 14:48:36 -0600

    starting day5

---
+++ day5_clau.py
@@ -0,0 +1,21 @@
+#1 Declare and empty list
+listA1 = []
+print(listA1)
+
+#2 Declare a list with more than 5 items
+listA2 = [1, 9, 78, 6, 36]
+print(listA2)
+
+#3 Find the length of your list
+print("La lista 2 tiene", len(listA2), " elementos")
+
+#4 Get the first item, the middle item and the last item of the list
+print("El primer número de la lista 2 es: ", listA2[0])
+print("El número del medio de la lista 2 es: ", listA2[2])
+print("El último número de la lista 2 es: ", listA2[-1])
+
+#5 Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixedDataTypes = ["Claudia, 18, 1.60, soltera, Santa Fe #259"]
+print(mixedDataTypes)
+
+#6 Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
