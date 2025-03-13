875a79152f59eb788904d305948a33b2e90aacc1
UP240462 <up240462@alumnos.upa.edu.mx>
2025-03-12 20:36:23 -0600

    end

--- day7_clau.py
+++ day7_clau.py
@@ -21,24 +21,69 @@ print(itCompanies.pop())
 #En cambio discard no arrojará nada
 
 #Exercises: Level 2
-#1 Join A and B
-set1 = {'A'}
-set2 = {'B'}
-set1.update(set2)
+#Join A and B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+A.update(B)
+print(A)
 
-#2 Find A intersection B
+#Find A intersection B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+A.intersection(B)
+print(A)    
 
+#Is A subset of B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+A.issubset(B)
 
-#3 Is A subset of B
+#Are A and B disjoint sets
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+print(A.isdisjoint(B))
 
+#Join A with B and B with A
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+union1 = A.union(B)
+union2 = B.union(A)
+print("A with B: ", union1)
+print("B with A: ", union2)
 
-#4 Are A and B disjoint sets
+#What is the symmetric difference between A and B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+(print(A.symmetric_difference(B)))
 
+#Delete the sets completely
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+del A
+del B
 
-#5 Join A with B and B with A
+#Exercises: Level 3
+#Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+setAge = set(age)
+print(len(age))
+print(len(setAge))
+if len(age) > len(setAge):
+    print("La lista mayor es: ", age)
+if len(age) < len(setAge):
+    print("El set mayor es: ", setAge)
 
+#Explain the difference between the following data types: string, list, tuple and set
+print("Un String es una cadena de caracteres")
+print("Una lista es una estructura de datos que almacena elementos de manera ordnada")
+print("Un tuple es lo mismo que une lista pero no se puede modificar")
+print("Un set es una colección de elementos que; no se repiten y no tienen un orden y los sets"" " 
+"son inmutables")
 
-#6 What is the symmetric difference between A and B
-
-
-#7 Delete the sets completely
+#I am a teacher and I love to inspire and teach people. 
+# How many unique words have been used in the sentence? 
+# Use the split methods and set to get the unique words.
+word = "I am a teacher and I love to inspire and teach people"
+totalWords = word.split()
+print("Las palabras unicas son: ", len(totalWords))
+print("Las palabras unicas son; ", totalWords)
