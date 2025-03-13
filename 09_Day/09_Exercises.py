392e39eeb0a010c5d2ad98f6d429663785352261
UP240462 <up240462@alumnos.upa.edu.mx>
2025-03-12 20:59:39 -0600

    start

---
+++ 09_Day/09_Exercises.py
@@ -0,0 +1,3 @@
+                                        #Exercises: Level 1
+#Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: 
+#You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
