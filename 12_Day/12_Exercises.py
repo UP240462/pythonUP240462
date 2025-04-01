beec188649a3930f6bf7502141d69b9873ac13ba
UP240462 <up240462@alumnos.upa.edu.mx>
2025-04-01 17:09:48 -0600

    .

---
+++ 12_Day/12_Exercises.py
@@ -0,0 +1,14 @@
+                                         #Exercises: Level 1
+#1 Write a function which generates a six digit/character random_user_id.
+from modulos import randomUserID
+print(randomUserID())
+
+#2 Modify the previous task. Declare a function named user_id_gen_by_user. 
+# It doesn’t take any parameters but it takes two inputs using input(). 
+# One of the inputs is the number of characters and the second input is the number of 
+# IDs which are supposed to be generated.
+from modulos import userByUser
+print(userByUser())
+
+#3 Write a function named rgb_color_gen. It will generate rgb colors
+#  (3 values ranging from 0 to 255 each).
