b1315ab7000fb552363b001b394d8497fe3063f8
UP240462 <up240462@alumnos.upa.edu.mx>
2025-04-01 20:32:18 -0600

    st

---
+++ 14_Day/14_Exercises.py
@@ -0,0 +1,18 @@
+                                     #Exercises: Level 1
+#1 Explain the difference between map, filter, and reduce.
+
+
+#2 Explain the difference between higher order function, closure and decorator
+
+
+#3 Define a call function before map, filter or reduce, see examples.
+
+
+#4 Use for loop to print each country in the countries list.
+
+
+#5 Use for to print each name in the names list.
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+list(map(lambda name: print(name), names))
+
+#6 Use for to print each number in the numbers list.
